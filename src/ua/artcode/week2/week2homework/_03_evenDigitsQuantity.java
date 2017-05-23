b2da19dd45e62929600f49c3beb487741f02fdcc
StarmanRS <rskorokhod@gmail.com>
2017-05-23 19:58:42 +0300

    week 2 homework

--- src/ua/artcode/week2/week2homework/_03_evenDigitsQuantity.java
+++ src/ua/artcode/week2/week2homework/_03_evenDigitsQuantity.java
@@ -20,13 +20,20 @@ public class _03_evenDigitsQuantity {
 
         //outer loop is for string element iteration
         for (int i = 0; i < snum.length(); i++) {
-            //inner loop is for check for accordance with even symbol
+/*            //inner loop is for check for accordance with even symbol
             for (int j = 0; j <= 8; j += 2) {
                 if (!((snum.charAt(i) + "").equals("0")) && (snum.charAt(i) + "").equals(j + "")) {
                     counter++;
                     break;
                 }
+            }*/
+            if (!(String.valueOf(snum.charAt(i)).equals("0")) &&
+                 (String.valueOf(snum.charAt(i)).equals("2") ||
+                  String.valueOf(snum.charAt(i)).equals("4") ||
+                  String.valueOf(snum.charAt(i)).equals("8"))) {
+                counter++;
             }
+
         }
 
         System.out.println("Amount of even digits in generated number is " + counter);
---
+++ src/ua/artcode/week2/week2homework/studyStudyStudy.java
@@ -0,0 +1,10 @@
+package ua.artcode.week2.week2homework;
+
+/**
+ * Created by Lucas_Starman on 23.05.2017.
+ */
+public class studyStudyStudy {
+    public static void main(String[] args) {
+
+    }
+}
